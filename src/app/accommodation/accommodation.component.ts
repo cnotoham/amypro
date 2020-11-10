4940105647a9ffe742fc6a0b4cb02fd357919b47
Cornelius Notohamiprodjo <cnotoham@gmail.com>
2020-11-11 00:36:57 +1100

    implement fontawesome and accommodation screen

--- src/app/accommodation/accommodation.component.ts
+++ src/app/accommodation/accommodation.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-accommodation',
   templateUrl: './accommodation.component.html',
   styleUrls: ['./accommodation.component.scss']
 })
 export class AccommodationComponent implements OnInit {
+
+  faCheckCircle = faCheckCircle;
 
   constructor() { }
 
