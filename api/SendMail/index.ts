3fbcdea0d29120afcdb7608291a581a851eed782
Cornelius Notohamiprodjo <cnotoham@gmail.com>
2021-01-11 17:47:57 +1100

    initial draft implementation of sendgrid

--- api/SendMail/index.ts
+++ api/SendMail/index.ts
@@ -1,17 +1,46 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as SendGrid from '@sendgrid/mail';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    if (req.body && req.body.name) {
+      context.log(`apiKey: ${process.env.apiKey}`);
+      SendGrid.setApiKey(process.env.apiKey);
+      const msg = {
+        to: 'cnotoham@yahoo.com.au',
+        from: 'webmaster <cnotoham@yahoo.com.au>',
+        subject: escape(req.body.subject),
+        text: escape(req.body.message)
+      };
+
+      try {
+        const resp = await SendGrid.send(msg);
+      } catch (error) {
+        if (error.response) {
+          context.log(`An error has occurred: ${ error.response.body }`);
+          context.res = {
+            status: 500,
+            body: `An error has occurred: ${ error.response.body }`
+          };
+          return;
+        }
+        context.res = {
+          status: 500,
+          body: 'An error has occurred'
+        };
+        return;
+      }
+
+      context.res = {
+          // status: 200, /* Defaults to 200 */
+          body: req.body
+      };
+      return;
+    }
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+      status: 400,
+      body: 'Invalid request'
+  };
 };
 
 export default httpTrigger;
